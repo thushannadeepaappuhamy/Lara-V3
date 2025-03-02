60764b9cef1538449fa6b7e570af03cb4f12896c
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2025-03-03 04:57:04 +0530

    Lααɾα-ᴍᴅ ✻

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "miZAWJRY#d2FrhrxFJyV4NbRq5pBrI1de9D8sKgGHQAJHhhH1mP4", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
